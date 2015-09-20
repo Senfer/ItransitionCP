a4107bccc9eb8b6c51c018b267649c1445dba2b0
Senfer <Soulstonesplinter@gmail.com>
2015-09-20 22:49:57 +0300

    Compability
    sus

--- CourseProject/Global.asax.cs
+++ CourseProject/Global.asax.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
+
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -20,7 +20,6 @@ namespace CourseProject
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
